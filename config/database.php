bc553070acc2e76c9b2f712044da3834897fca81
AnguandiaJered <76424216+AnguandiaJered@users.noreply.github.com>
2022-11-21 19:02:14 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
-$db_name='alvine-osongo_caritasdb';
-$db_user='287598_dbcaritas';
-$db_password='ALVINEGOMA19102017';
+$db_name='lucifer_caritas';
+$db_user='lucifer_uwonda';
+$db_password='@@1980@@';
 
 $db=new PDO("mysql:host=localhost;dbname=".$db_name,$db_user,$db_password);
 
