f4082ec5286b619aff70b131c99febfc0b32cee6
AnguandiaJered <anguandiatsandijered03@gmail.com>
2022-11-21 18:18:19 +0200

    caritas manager

---
+++ config/database.php
@@ -0,0 +1,8 @@
+<?php
+$db_name='alvine-osongo_caritasdb';
+$db_user='287598_dbcaritas';
+$db_password='ALVINEGOMA19102017';
+
+$db=new PDO("mysql:host=localhost;dbname=".$db_name,$db_user,$db_password);
+
+?>
